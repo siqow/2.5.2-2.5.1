6a0af08c5a8da53638faf7327c5ff65708e23a0b
siqow <152678667+siqow@users.noreply.github.com>
2024-10-31 13:24:09 -0700

    Update Player.java

--- Player.java
+++ Player.java
@@ -4,46 +4,46 @@
  * A Player class the PhraseSolverGame
  */
 
-import java.util.Scanner;
+ import java.util.Scanner;
 
-public class Player
-{
-  /* your code here - attributes */
-  private String name = ""; 
-  private double points;
-  
-  /* your code here - accessor(s) */ 
-  public String getName()
-  {
-    return name;
-  }
-  public double getPoints()
-  {
-    return points;
-  }
-  /* your code here - constructor(s) */ 
-  
-  public Player()
-  {
-    points = 0;
-    System.out.println("Enter Player Name:");
-    Scanner sc = new Scanner(System.in);
-    String newName = sc.nextLine();
-    name = newName;
-    System.out.println("Hello and welcome to the game " + name);
-
-  }
-  public Player(String inputName){
-    name = inputName;
-    points = 0;
-  }
-  /* your code here - mutator(s) */ 
-  public void setName(String x)
-  {
-    name = x;
-  }
-
-  public void setPoints(int x) { 
-    points = x;
-  }
-}
+ public class Player
+ {
+   /* your code here - attributes */
+   private String name = ""; 
+   private double points;
+   
+   /* your code here - accessor(s) */ 
+   public String getName()
+   {
+     return name;
+   }
+   public double getPoints()
+   {
+     return points;
+   }
+   /* your code here - constructor(s) */ 
+   
+   public Player()
+   {
+     points = 0;
+     System.out.println("Enter Player Name:");
+     Scanner sc = new Scanner(System.in);
+     String newName = sc.nextLine();
+     name = newName;
+     System.out.println("Hello and welcome to the game " + name);
+ 
+   }
+   public Player(String inputName){
+     name = inputName;
+     points = 0;
+   }
+   /* your code here - mutator(s) */ 
+   public void setName(String x)
+   {
+     name = x;
+   }
+ 
+   public void setPoints(int x) { 
+     points = x;
+   }
+ }
